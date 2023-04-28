5dddf16c13ced15ec1f5ec634b1a3f8b266d3dc8
emamingio22 <emawankenoby02@gmail.com>
2023-04-28 14:25:37 +0200

    attributi e costruttore

--- GestoreEventi/ProgrammaEventi.cs
+++ GestoreEventi/ProgrammaEventi.cs
@@ -8,6 +8,17 @@ namespace GestoreEventi
 {
     public class ProgrammaEventi
     {
+        // ATTRIBUTI
+        private string titolo;
+        private List<Evento> eventi;
+
+        // COSTRUTTORE
+        public EventiProgramma(string titolo, List<Evento> eventi)
+        {
+            this.titolo = titolo;
+            eventi = new List<Evento>();
+
+        } 
 
     }
 }
