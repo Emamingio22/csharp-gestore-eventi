d41a6a857933eba25f264b5dc28c42e8be98ba6d
emamingio22 <emawankenoby02@gmail.com>
2023-04-28 11:48:23 +0200

    chiedo l'evento, data, max capienza e posti da prenotare

--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GestoreEventi;
+
+//------evento------
+Console.Write("Inserisci l'evento: ");
+string nomeEvento = Console.ReadLine();
+
+Console.Write("Inserisci la data: ");
+DateTime oraEvento = DateTime.Parse(Console.ReadLine());
+
+Console.Write("Inserisci la capienza massima: ");
+int maxCapienza = int.Parse(Console.ReadLine());
+
+Evento concerto = new Evento (nomeEvento, oraEvento, maxCapienza);
+
+Console.Write("Quanti posti vuoi prenotare? ");
+int postiPrenotati = int.Parse(Console.ReadLine());
+
